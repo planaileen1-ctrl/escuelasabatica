26b5e7f9723ef9135ff227c6dcf8d9270feef4ef
Raul Leon <planaileen1@gmail.com>
2026-03-07 03:39:19 -0500

    super

--- components/PDFViewer.tsx
+++ components/PDFViewer.tsx
@@ -1,28 +1,20 @@
-"use client"
+import { Worker, Viewer } from '@react-pdf-viewer/core';
+import { scrollModePlugin } from '@react-pdf-viewer/scroll-mode';
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import '@react-pdf-viewer/scroll-mode/lib/styles/index.css';
 
-import { Document, Page, pdfjs } from "react-pdf"
-import { useState } from "react"
+export default function PdfViewer({ url }: { url: string }) {
+  const scrollModePluginInstance = scrollModePlugin();
 
-pdfjs.GlobalWorkerOptions.workerSrc = "/pdf.worker.min.mjs"
-
-export default function PDFViewer({ file }: { file: string }) {
-  const [numPages, setNumPages] = useState<number>(0)
-
-  function onLoadSuccess({ numPages }: { numPages: number }) {
-    setNumPages(numPages)
-  }
-
-    return (
-      <div className="flex-1 h-full overflow-auto touch-pan-y">
-        <Document
-          file={file}
-          onLoadSuccess={onLoadSuccess}
-          loading="Cargando PDF..."
-        >
-          {Array.from(new Array(numPages), (_, i) => (
-            <Page key={i} pageNumber={i + 1} width={700} />
-          ))}
-        </Document>
-      </div>
-    )
+  return (
+    <div className="flex-1 h-full overflow-auto touch-pan-y">
+      <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.7.107/build/pdf.worker.min.js">
+        <Viewer
+          fileUrl={url}
+          defaultScale={1.0}
+          plugins={[scrollModePluginInstance]}
+        />
+      </Worker>
+    </div>
+  );
 }
