f11894a9117bd4ed7ee25d4cf98cd9b2ab727036
Raul Leon <planaileen1@gmail.com>
2026-03-07 07:27:26 -0500

    super 3

--- components/PDFViewer.tsx
+++ components/PDFViewer.tsx
@@ -1,19 +1,11 @@
 import { Worker, Viewer } from '@react-pdf-viewer/core';
-import { scrollModePlugin } from '@react-pdf-viewer/scroll-mode';
 import '@react-pdf-viewer/core/lib/styles/index.css';
-import '@react-pdf-viewer/scroll-mode/lib/styles/index.css';
 
 export default function PdfViewer({ url }: { url: string }) {
-  const scrollModePluginInstance = scrollModePlugin();
-
   return (
-    <div className="flex-1 h-full overflow-auto touch-pan-y">
-      <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.7.107/build/pdf.worker.min.js">
-        <Viewer
-          fileUrl={url}
-          defaultScale={1.0}
-          plugins={[scrollModePluginInstance]}
-        />
+    <div style={{ height: '100vh' }}>
+      <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
+        <Viewer fileUrl={url} />
       </Worker>
     </div>
   );
